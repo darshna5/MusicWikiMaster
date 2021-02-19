c9d34d17ff341d04917c9c32a8859d1fd1dd6ebb
darshnakumari <darshna.a.kumari@accenture.com>
2021-02-19 12:03:17 +0530

    change user name

--- app/src/main/java/com/example/musicwikimaster/utility/Constants.kt
+++ app/src/main/java/com/example/musicwikimaster/utility/Constants.kt
@@ -1,10 +1,10 @@
 package com.example.musicwikimaster.utility
 
 object Constants {
-  const val API_KEY = "35066a49f2deb23a3c35fd48ff5c9869"
+  const val API_KEY = "451871b9fc8919b6f0c64fd790d9641b"
   var BASE_URL = "http://ws.audioscrobbler.com/2.0/"
   const val TOP_ITEMS_LIMIT = 10
-  const val DEFAULT_LASTFM_USER = "drrobbins"
+  const val DEFAULT_LASTFM_USER = "Darshna09"
   const val TAG_NAME = "TAG_NAME"
 
 }
